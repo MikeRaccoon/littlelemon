dcba352709eaff91b2454309455294797f823901
Mike <cyberenot@gmail.com>
2023-02-22 19:03:43 -0800

    set-up-the-onboarding-page

---
+++ Little Lemon/Little Lemon/Onboarding.swift
@@ -0,0 +1,49 @@
+//
+//  Onboarding.swift
+//  Little Lemon
+//
+//  Created by Mike on 2023-02-22.
+//
+
+import SwiftUI
+
+let kFirstName = "first name key"
+let kLastName = "last name key"
+let kEmail = "email key"
+
+struct Onboarding: View {
+    @State var firstName: String = ""
+    @State var lastName: String = ""
+    @State var email: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("First name", text: $firstName)
+            TextField("Last name", text: $lastName)
+            TextField("Email", text: $email)
+            
+            Button(action: {
+                if !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty,
+                   isValidEmailFormat(email) {
+                    UserDefaults.standard.set(firstName, forKey: kFirstName)
+                    UserDefaults.standard.set(lastName, forKey: kLastName)
+                    UserDefaults.standard.set(email, forKey: kEmail)
+                    print("done")
+                }
+            }) {
+                Text("Register")
+            }
+        }
+    }
+    
+    func isValidEmailFormat(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
