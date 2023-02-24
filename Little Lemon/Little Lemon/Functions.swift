5e321fcb8f9762676e6d88d781740f502573da29
Mike <mraccoondev@gmail.com>
2023-02-24 02:53:49 -0800

    little-lemon-food-ordering-app

---
+++ Little Lemon/Little Lemon/Functions.swift
@@ -0,0 +1,8 @@
+//
+//  Functions.swift
+//  Little Lemon
+//
+//  Created by Mike on 2/24/23.
+//
+
+import Foundation
---
+++ Little Lemon/Little Lemon/Header.swift
@@ -0,0 +1,20 @@
+//
+//  Header.swift
+//  Little Lemon
+//
+//  Created by Mike on 2/24/23.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Header_Previews: PreviewProvider {
+    static var previews: some View {
+        Header()
+    }
+}
